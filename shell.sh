835dbda4dd2d813e462f8da316575cf144b5f47b
Jinoh Kang <luke1337@theori.io>
2026-09-09 05:16:07 +0000

    image1

--- shell.sh
+++ shell.sh
@@ -7,9 +7,13 @@ cmd=$cmd'echo "ubuntu ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/99-ubuntu; '
 cmd=$cmd'mountpoint -q /home/ubuntu || mount --bind /var/lib/docker/codespacemount/workspace/ubuntu /home/ubuntu; '
 cmd=$cmd'exec systemd-run --quiet --pty --wait --collect --service-type=exec --setenv=TERM="$TERM" --uid=ubuntu --gid=ubuntu --working-directory=/home/ubuntu "$@"'
 
-exec sudo docker -H unix:///var/run/docker-host.sock \
+export DOCKER_HOST=unix:///var/run/docker-host.sock
+
+image=$(docker container ls --filter=volume=/var/run/docker-host.sock --format='{{.Image}}')
+
+exec docker \
     run --rm -it --name="guest2host-$$" --privileged \
     --pid=host --userns=host --uts=host \
     --ipc=host --cgroupns=host --network=host \
-    -v /:/host mcr.microsoft.com/devcontainers/base:ubuntu24.04 \
+    -v /:/host "$image" \
     nsenter -FZ -t1 -a env TERM="$TERM" sh -ec "$cmd" - "$@"
